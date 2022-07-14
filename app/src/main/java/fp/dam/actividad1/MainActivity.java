273180babbda869fbbc85a54523e214043c39d16
angela <angelablancoroca@gmail.com>
2022-07-14 22:01:56 +0200

    Video 4 Syncronización del crono

--- app/src/main/java/fp/dam/actividad1/MainActivity.java
+++ app/src/main/java/fp/dam/actividad1/MainActivity.java
@@ -23,25 +23,87 @@ import android.widget.ImageButton;
             //Poner el id adecuado a cada boton
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+// --------- VIDEO - 4-----------------
+
+            // Opciones diferentes para abrir la activity
+
+public class MainActivity extends AppCompatActivity /*implements View.OnClickListener */{
 
 
-
+    //ATRIBUTOS
+    ImageButton botonCro;
+    ImageButton botonConta;
+    /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //OPCION 1 --- podría usar las varibles anteriores declaradas -- atributos
+
         //Primero necesito el botón
         ImageButton botonCrono = (ImageButton) findViewById(R.id.botonCrono);
         //Luego necesito el listener, paso como parametro a un objeto que implemete la interfaz ViewOnClick...
         botonCrono.setOnClickListener(this);
+        //El otro botón -- uso el mismo objeto
+        botonCrono = (ImageButton) findViewById(R.id.botonCont);
+        botonCrono.setOnClickListener(this);
+
+        //OPCION 2
+
+        //HACERLO SIN LA VARIABLE IMAGENBUTTON
+        ((ImageButton) findViewById(R.id.botonCrono)).setOnClickListener(this);
+        ((ImageButton) findViewById(R.id.botonCont)).setOnClickListener(this);
     }
 
 
     //Lo que tiene que ocurrir al pulsar el botón, que se abra la activity
+    //Con un if miro que botón estoy pulsando
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(getApplicationContext(),cronoActivity.class); //Recibe como parametro la activity que recibe la accion
-        startActivity(intent);
+        if (v.getId() == R.id.botonCrono) {
+            Intent intent = new Intent(getApplicationContext(), cronoActivity.class); //Recibe como parametro la activity que recibe la accion
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(getApplicationContext(), contadorActivity.class); //Recibe como parametro la activity que recibe la accion
+            startActivity(intent);
+        }
+
+        // CON LA OPCION DE LOS ATRIBUTOS
+        /*
+        if(v == botonCro){
+            Intent intent = new Intent(getApplicationContext(),cronoActivity.class); //Recibe como parametro la activity que recibe la accion
+            startActivity(intent);
+        }
+        else{
+            Intent intent = new Intent(getApplicationContext(),contadorActivity.class); //Recibe como parametro la activity que recibe la accion
+            startActivity(intent);
+        }
+         */
+
+
+        //OPCION 3 REFERENCIAS A METODOS
+
+        //Se asigna el metodo en objeto del XML
+
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            botonCro = (ImageButton) findViewById(R.id.botonCrono);
+
+            botonConta = (ImageButton) findViewById(R.id.botonCont);
+        }
+
+
+        public void responderPulsacionCrono (View v){
+            Intent intent = new Intent(getApplicationContext(), cronoActivity.class); //Recibe como parametro la activity que recibe la accion
+            startActivity(intent);
+        }
+
+        public void responderPulsacionConta (View v){
+            Intent intent = new Intent(getApplicationContext(), contadorActivity.class); //Recibe como parametro la activity que recibe la accion
+            startActivity(intent);
+        }
     }
-}
--- app/src/main/java/fp/dam/actividad1/cronoActivity.java
+++ app/src/main/java/fp/dam/actividad1/cronoActivity.java
@@ -2,13 +2,77 @@ package fp.dam.actividad1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+//--------------- VIDEO -4 ---------------------------------------
+
 
 public class cronoActivity extends AppCompatActivity {
+
+    private Button botonIniciar;
+    private Button botonLapso;
+    private boolean iniciado;
+    private TextView crono;
+    private long lapso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crono);
+        botonIniciar = (Button) findViewById(R.id.iniciar);
+        botonLapso = (Button) findViewById(R.id.reiniciar);
+        crono = findViewById(R.id.textCrono);
+    }
+
+
+    //Metodo para mostrar formateado el tiempo
+    private void mostrartiempo(){
+        synchronized (this){
+            crono.setText(String.valueOf(lapso));
+        }
+
+    }
+
+    //Estaria el bucle del hilo
+    private void cronometrar(){
+        iniciado=true;
+        long to, t1;
+        lapso=0; //Guardo el tiempo transcurrido
+        System.nanoTime(); //Precision de nanosegundos
+        to = System.currentTimeMillis();//Precision milisegundos
+        while(iniciado){
+                t1= System.currentTimeMillis();
+                synchronized (this){
+                    lapso += t1-to; //Obtengo el tiempo desde que comenzo hasta la primera iteraccion;
+                }
+                to=t1; // Establezco el punto de partida donde comenzó
+
+                 //crono.setText(String.valueOf(lapso)); //Desde aquí no funciona, tendría que ser desde otro hilo de la cola de eventos
+                if(lapso>0){
+                    //Ejecutar en el hilo de la cola de eventos
+                    runOnUiThread(this::mostrartiempo); // --------->>>>>> NO SE ESTÁ CREANDO UN HILO NUEVO
+                }
+        }
+    }
+
+    //Metodo para iniciar el cronometro -- uso un hilo
+    public void iniciarParar(View v){
+        if(!iniciado){
+            //Creo el hilo y lo ejecuto
+            new Thread (this::cronometrar).start();
+        }
+        else{
+
+        }
+    }
+
+    public void lapso(View v){
+
     }
 }
